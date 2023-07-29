7eed9d3bb852817f792c5b84c03feadf9387afb2
slas <139346466+kanpokunp@users.noreply.github.com>
2023-07-29 20:31:39 +0700

    last update

--- kodetimbangan/kodetimbangan.ino
+++ kodetimbangan/kodetimbangan.ino
@@ -1,32 +1,41 @@
-#include <HX711.h> //Include Library HX711 Bogdan Necula
 #include <Wire.h>  //Include Library Wire untuk I2C
 #include <LiquidCrystal_I2C.h>  //Include Library LiquidCrystal_I2C untuk LCD
+#include <HX711.h> //Include Library HX711 Bogdan Necula
 
 HX711 timbangan; //deklarasikan instance dari class HX711, bernama 'timbangan'.
-const float calibration_factor = 1000.0; //atur sesuai kebutuhan.
+const float kalibrasi = 1200.0; //atur sesuai kebutuhan.
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  //Inisialisasikan modul LCD dengan I2C address and besar LCD, yakni 16x2
 
 void setup() { //setup board Arduino
-  lcd.begin(16, 2);  //Initialisasikan LCD
-  
-  //initialisasikan timbangan dengan HX711
+  lcd.init();
+  lcd.backlight();
+
+   //inisialisasikan timbangan dengan HX711
   timbangan.begin(2, 3);  //Koneksikan DT ke pin 2, dan SCK ke pin 3
-  timbangan.set_scale(calibration_factor); //set faktor calibration dari timbangan
+  timbangan.set_scale(kalibrasi); //set faktor kalibrasi dari timbangan
   timbangan.tare();  //kosongkan timbangan kembali ke 0;
-  
+
+  // tambah delay 100 milidetik agar sensor bisa stabil dahulu
+  delay(100);
+
   lcd.setCursor(0, 0); //set cursor LCD ke pojok kiri atas
-  lcd.print("Weight: "); //print "Weight: " pada LCD
+  lcd.print("Berat: "); //print "Weight: " pada LCD
 }
 
 void loop() { //loop yang terus berjalan ketika arduino digunakan
 
-  float berat = timbangan.get_units(); //baca nilai berat dari strain gauge, melalui HX711 Amplifier, berupa gram
-  berat /= 1000; //konversikan berat dari gram ke kilogram
+  float berat_gram = timbangan.get_units(); //baca berat dari sensor, berupa gram
 
-  lcd.setCursor(8, 0);
-  //output hasil nilai berat ke LCD
-  lcd.print(berat, 2); //tampilkan sebagai desimal dua digit
+  if (berat_gram < 0) {
+    berat_gram = 0;   //pastikan berat tidak muncul negatif
+  }
+
+  float berat_kg = berat_gram / 1000; //konversikan berat dari gram ke kilogram
+
+  lcd.setCursor(8, 0); //output hasil nilai berat ke LCD
+  lcd.print(berat_kg, 3); //tampilkan sebagai desimal dua digit
   lcd.print(" kg");
-  delay(1000);  //Tunggu sekitar 1 detik sebelum melakukan pengukuran berikutnya
-} 
+  
+  delay(500);  //Tunggu sekitar 0.5 detik sebelum melakukan pengukuran berikutnya
+}
